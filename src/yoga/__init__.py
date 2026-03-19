5f632484082990473ebee2852cd99f39c4e02136
jstzwj <1103870790@qq.com>
2026-03-19 20:20:22 +0800

    feat: Bump version to v0.1.0 for release

--- src/yoga/__init__.py
+++ src/yoga/__init__.py
@@ -5,6 +5,8 @@ This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
 
+__version__ = "0.1.0"
+
 from .YGConfig import *
 from .YGEnums import *
 from .YGNode import *
